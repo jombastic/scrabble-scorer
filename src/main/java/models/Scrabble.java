9442b107cdbdc27fe2db7fbb14f1d0b89985033c
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-04-01 17:11:08 +0200

    Add logic for returning 1 for vowels or l, n, r, s, t letters

--- src/main/java/models/Scrabble.java
+++ src/main/java/models/Scrabble.java
@@ -4,7 +4,14 @@ import java.lang.reflect.Array;
 
 public class Scrabble {
   public Integer calculateScore(String word) {
-    //String[] score1Letters = {"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"};
-    return 1;
+    String lowerCaseWord = word.toLowerCase();
+    String[] score1Letters = {"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"};
+    Integer result = null;
+    for (String letter : score1Letters) {
+      if (lowerCaseWord.equals(letter)) {
+        result = 1;
+      }
+    }
+    return result;
   }
 }
