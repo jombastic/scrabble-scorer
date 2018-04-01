a24f7b489b510469d98bd8c89a9abcb94b5f5c16
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-04-01 16:26:15 +0200

    Add logic for returning score for a single letter

---
+++ src/main/java/models/Scrabble.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.lang.reflect.Array;
+
+public class Scrabble {
+  public Integer calculateScore(String word) {
+    //String[] score1Letters = {"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"};
+    return 1;
+  }
+}
