ca271b4957492c1e286cd3323d06fbf769760757
Slavcho Mitrov <slavcho_mitrov@yahoo.com>
2018-04-02 10:26:32 +0200

    Add logic for returning 2 for d,g letters

--- src/main/java/models/Scrabble.java
+++ src/main/java/models/Scrabble.java
@@ -1,15 +1,33 @@
 package models;
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
 import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Scrabble {
   public Integer calculateScore(String word) {
-    String lowerCaseWord = word.toLowerCase();
-    String[] score1Letters = {"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"};
-    Integer result = null;
-    for (String letter : score1Letters) {
-      if (lowerCaseWord.equals(letter)) {
-        result = 1;
+    char[] inputedCharacters = word.toLowerCase().toCharArray();
+
+    Map<Character, Integer> allLetters = new HashMap<>();
+    allLetters.put('a', 1);
+    allLetters.put('e', 1);
+    allLetters.put('i', 1);
+    allLetters.put('o', 1);
+    allLetters.put('u', 1);
+    allLetters.put('l', 1);
+    allLetters.put('n', 1);
+    allLetters.put('r', 1);
+    allLetters.put('s', 1);
+    allLetters.put('t', 1);
+    allLetters.put('d', 2);
+    allLetters.put('g', 2);
+
+    int result = 0;
+    for (char character : inputedCharacters) {
+      if (allLetters.containsKey(character)) {
+        result = allLetters.get(character);
       }
     }
     return result;
